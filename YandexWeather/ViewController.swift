807ae49b8ef88fab8c2c0b14bfa52bcd28b486d4
Valter4578 <mrvalter566@gmail.com>
2021-07-10 13:17:48 +0700

    Initial Commit

---
+++ YandexWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexWeather
+//
+//  Created by Максим Алексеев on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
